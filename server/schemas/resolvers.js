50ae55a0a2feb7f881be1dcc82ff7e467ef66bef
Chris Onions <c.onions101@gmail.com>
2021-07-27 21:21:40 +1000

    Added getUsers query. Added addUserand login mutation.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,35 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
-// jwt
-// const { signToken } = require('../utils/auth');
-
-// const stripe = require('stripe') for donations 
-// ('sk_test_4eC39HqLyjWDarjtT1zdp7dc'); test
+const { signToken } = require('../utils/auth');
+// jsonwebtoken
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    sayHi: () => 'hello world'
+    getUsers: async () => {
+      return await User.find({});
+    },
+  },
 
-    // user: async () => {
-    //   return await User.find();
-    // },
+  Mutation: {
+    addUser: async (_, { username, firstName, lastname, email, password }) => {
+      const user = await User.create({ username, firstName, lastname, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Sorry Try Again');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Sorry Try Again');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
   }
 }
+
 module.exports = resolvers;
